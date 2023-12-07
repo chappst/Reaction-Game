2a3f04dec00f08526864d54618f6db8835867e94
gillnim <150408416+gillnim@users.noreply.github.com>
2023-12-07 11:12:56 -0800

    Create update_scores.h

---
+++ update_scores.h
@@ -0,0 +1,8 @@
+#ifndef UPDATE_SCORES_H
+#define UPDATE_SCORES_H
+
+
+// Function to compare reaction times and update scores
+void updateScores(int reaction_time_player1, int reaction_time_player2, int *score_player1, int *score_player2);
+
+#endif /* UPDATE_SCORES_H */
