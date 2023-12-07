ba1859783cfc33612cb18b3b2639a1e43c790437
Chloe Thiessen <150405221+itschloethiessen@users.noreply.github.com>
2023-12-06 16:39:22 -0800

    Update and rename PrintToLED.h to JoystickSelection.h

---
+++ JoystickSelection.h
@@ -0,0 +1,12 @@
+#ifndef JOYSTICKSELECTION_H
+#define JOYSTICKSELECTION_H
+
+double Joystick_readX();
+double Joystick_readY();
+int JoystickDirection();
+
+void displayEasy();
+void displayMed();
+void displayHard();
+
+#endif /*JOYSTICKSELECTION_H*/
