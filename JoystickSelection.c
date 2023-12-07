a16e34296c5bc0c87b74226356c4c4d19efc311e
Chloe Thiessen <150405221+itschloethiessen@users.noreply.github.com>
2023-12-06 16:41:20 -0800

    Update JoystickSelection.c

--- JoystickSelection.c
+++ JoystickSelection.c
@@ -1,23 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <JoystickSelection.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <math.h>
 
+#define A2D_FILE_VOLTAGE2 "/sys/bus/iio/devices/iio:device0/in_voltage2_raw"
+#define A2D_FILE_VOLTAGE3 "/sys/bus/iio/devices/iio:device0/in_voltage3_raw"
 
-// Function definitions for displaying difficulty levels
-void displayEasy() {
-    printf("=== Easy Mode ===\n");
-    printf("Instructions for Easy Mode\n");
+double Joystick_readY(){
+    // Open file
+    FILE *f = fopen(A2D_FILE_VOLTAGE2, "r");
+    if (!f) {
+        printf("ERROR: Unable to open voltage input file. Cape loaded?\n");
+        printf(" Check /boot/uEnv.txt for correct options.\n");
+        exit(-1);
+    }
+
+    // Get reading
+    float a2dReading = 0;
+    int itemsRead = fscanf(f, "%f", &a2dReading);
+    if (itemsRead <= 0) {
+        printf("ERROR: Unable to read values from voltage input file.\n");
+        exit(-1);
+    }
+
+    // Close file
+    fclose(f);
+
+    a2dReading = ((2 * a2dReading) / (A2D_MAX_READING) - 1) *1;
+    return a2dReading;
 }
 
-void displayMed() {
-    printf("=== Medium Mode ===\n");
-    printf("Instructions for Medium Mode\n");
+double Joystick_readX(){
+    // Open file
+    FILE *f = fopen(A2D_FILE_VOLTAGE3, "r");
+    if (!f) {
+        printf("ERROR: Unable to open voltage input file. Cape loaded?\n");
+        printf(" Check /boot/uEnv.txt for correct options.\n");
+        exit(-1);
+    }
+
+    // Get reading
+    float a2dReading = 0;
+    int itemsRead = fscanf(f, "%f", &a2dReading);
+    if (itemsRead <= 0) {
+        printf("ERROR: Unable to read values from voltage input file.\n");
+        exit(-1);
+    }
+
+    // Close file
+    fclose(f);
+
+    a2dReading = ((2 * a2dReading) / (A2D_MAX_READING) - 1) *1;
+    return a2dReading;
 }
 
-void displayHard() {
-    printf("=== Hard Mode ===\n");
-    printf("Instructions for Hard Mode\n");
+// Function to check Joystick Direction
+int JoystickDirection(){
+    double x = Joystick_readX();
+    double y = Joystick_readY();
+
+    if (x < -0.5) {
+        return 1; // Right
+    } else if (x > 0.5) {
+        return 2; // Left
+    } else if (y > 0.5) {
+        return 3; // Down
+    } else if (y < -0.5) {
+        return 4; // Up
+    } else if (x == 0 && y == 0){
+        return 0; //Centred
+    } else{
+        return 0;//(rand() % 4) + 1; // Arbitrarily chosen
+    }
 }
+
 
 int choice = 0;
 int difficulty = -1;
@@ -72,5 +134,21 @@ int prevChoice = -1;
 
     return 0;
 
+
+// Function definitions for displaying difficulty levels
+void displayEasy() {
+    printf("=== Easy Mode ===\n");
+    printf("Instructions for Easy Mode\n");
+}
+
+void displayMed() {
+    printf("=== Medium Mode ===\n");
+    printf("Instructions for Medium Mode\n");
+}
+
+void displayHard() {
+    printf("=== Hard Mode ===\n");
+    printf("Instructions for Hard Mode\n");
+}
 
 
