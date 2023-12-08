94321c19949df6251779b6ef8dd5b9862c4cd388
Chloe Thiessen <150405221+itschloethiessen@users.noreply.github.com>
2023-12-07 17:38:32 -0800

    Update buttonPressed.h

--- buttonPressed.h
+++ buttonPressed.h
@@ -1,8 +1,8 @@
 #ifndef BUTTON_PRESSED_H_
 #define BUTTON_PRESSED_H_
 
-static void *greenButtonReader(void *_)
-static void *redButtonReader(void *_)
+static void greenButtonReader()
+static void redButtonReader()
 static char readButtonValue(char *fileName)
 static bool isClicked(char *fileName){
 static void sleepForMs(long long delayInMs)
